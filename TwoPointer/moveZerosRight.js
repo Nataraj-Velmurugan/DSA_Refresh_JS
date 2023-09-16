64dcec3233c008bd3e008d6deb8ef7b10ed02a09
Nataraj Velmurugan <raajei50@gmail.com>
2023-09-16 09:42:46 +0530

    Update moveZerosRight.js

--- TwoPointer/moveZerosRight.js
+++ TwoPointer/moveZerosRight.js
@@ -3,22 +3,18 @@
 let nums = [3, 0, 4, 5, 0, 0, 1];
 
 let moveZeros = (nums) => {
-    let left = 0, right = 1;
+    let left = 0, right = 0;
 
     while (right < nums.length) {
         let temp;
-        if(nums[left] > nums[right]) {
-            left++;
-            right++;
-        }
-        else if(nums[left] < nums[right]) {
-            temp = nums[left];
+        if( nums[right] !=0 ) {
+           temp = nums[left];
             nums[left] = nums[right]
             nums[right] = temp
             left++;
-            right++;
         }
-        else right++;
+        right++;
+        console.log(nums)
     }
 
     return nums
