d9da56ba54b8e7647dba35cb429e2f455eefeeac
Nataraj Velmurugan <raajei50@gmail.com>
2023-09-16 07:11:10 +0530

    Update reverseVowel.js

--- TwoPointer/reverseVowel.js
+++ TwoPointer/reverseVowel.js
@@ -7,7 +7,7 @@ let vowel = ['a', 'e', 'i', 'o', 'u'];
 let left = 0, right = chars.length - 1;
 
 let reverseVowel = (chars, vowel) => {
-    while (left < right ) {
+    while (left < right ) { 
         let temp;
         if(vowel.includes(chars[left]) && vowel.includes(chars[right])) {
             temp = chars[left];
