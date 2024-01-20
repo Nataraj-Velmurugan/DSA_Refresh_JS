a51ba8bc73ce27cd3c1795dd361416fa208230f6
Nataraj Velmurugan <raajei50@gmail.com>
2024-01-20 10:34:11 +0530

    Update duplicateZeros.js

--- TwoPointer/duplicateZeros.js
+++ TwoPointer/duplicateZeros.js
@@ -1,1 +1,22 @@
+https://leetcode.com/problems/duplicate-zeros/description/
 
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+
+let arr = [1,2,3];
+
+var duplicateZeros = function(arr) {
+
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] === 0) {
+            arr.splice(i+1, 0, 0)
+            arr.pop()
+            i++
+        }
+    }
+
+    return arr
+    
+};
+
+console.log(duplicateZeros(arr))
