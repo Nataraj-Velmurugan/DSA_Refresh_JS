090d4e504c4aba9cfa53031a377930da8bbd6921
Nataraj Velmurugan <raajei50@gmail.com>
2024-03-23 13:26:58 +0530

    Create CountofSubsetswithgivenDifference.js

---
+++ DP/knapsack/CountofSubsetswithgivenDifference.js
@@ -0,0 +1,13 @@
+// Input: N = 4, arr[] = [5, 2, 6, 4], diff = 3
+// Output: 1
+// Explanation: We can only have a single partition which is shown below:
+// {5, 2} and {6, 4} such that S1 = 7 and S2 = 10 and thus the difference is 3
+
+// Input: N = 5, arr[] = [1, 2, 3, 1, 2], diff = 1
+// Output: 5
+// Explanation: We can have five partitions which is shown below
+// {1, 3, 1} and {2, 2} – S1 = 5, S2 = 4
+// {1, 2, 2} and {1, 3} – S1 = 5, S2 = 4
+// {3, 2} and {1, 1, 2} – S1 = 5, S2 = 4
+// {1, 2, 2} and {1, 3} – S1 = 5, S2 = 4
+// {3, 2} and {1, 1, 2} – S1 = 5, S2 = 4
